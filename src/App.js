0fca00ff5288bb180e87830442a5f0000b3ce157
Michael Chilaka <obichilaka@gmail.com>
2022-09-28 08:28:51 +0100

    understanding useEffect hook

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import './App.css';
 import StateTut from './components/useState/StateTut';
 import ReducerTut from './components/useReducer/ReducerTut';
+import EffectTut from './components/useEffect/EffectTut';
 
 function App() {
   return (
     <div className="app">
       {/* <StateTut /> */}
-      <ReducerTut />
+      {/* <ReducerTut /> */}
+      <EffectTut />
     </div>
   );
 }
--- src/components/useEffect/EffectTut.js
+++ src/components/useEffect/EffectTut.js
@@ -1,8 +1,37 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
+
+// useEffect hook is a function that's called whenever the page re-renders
 
 const EffectTut = () => {
+const [data, setData] = useState("");
+const [count, setCount] = useState(0)
+
+
+  // useEffect(() => {
+  //   const apiHub = "https://jsonplaceholder.typicode.com/comments";
+  //   fetch(apiHub)
+  //     .then((response) => {
+  //       console.log(response.data)
+  //     })
+  // })
+
+  useEffect(() => {
+    axios.get("https://jsonplaceholder.typicode.com/comments")
+      .then((response) => {
+        setData(response.data[0].email)
+        console.log("Api was called")
+      })
+  }, [])
+
   return (
-    <div></div>
+    <div>Hello 
+      <p>{data}</p> 
+    <p>{count}</p>
+    <button onClick={() => {
+      setCount(count + 1)
+    }}>Click me</button>
+    </div>
   )
 }
 
