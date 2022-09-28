e3069013776143bd6477cfb6722534ad8e5bdaeb
Michael Chilaka <obichilaka@gmail.com>
2022-09-28 08:50:30 +0100

    understanding useRef hook

--- src/App.js
+++ src/App.js
@@ -2,13 +2,15 @@ import './App.css';
 import StateTut from './components/useState/StateTut';
 import ReducerTut from './components/useReducer/ReducerTut';
 import EffectTut from './components/useEffect/EffectTut';
+import RefTut from './components/useRef/RefTut';
 
 function App() {
   return (
     <div className="app">
       {/* <StateTut /> */}
       {/* <ReducerTut /> */}
-      <EffectTut />
+      {/* <EffectTut /> */}
+      <RefTut />
     </div>
   );
 }
---
+++ src/components/useRef/RefTut.js
@@ -0,0 +1,26 @@
+import React, { useRef } from 'react'
+
+// useRef hook is for the manipulation of dom elements, just like is done on vanilla JS
+// when you would like to manipulate DOM elements like document.getElementById or document.getElementByClass
+
+const RefTut = () => {
+    const inputRef = useRef();
+
+    const handleClick = () => {
+        console.log(inputRef.current.value);  
+        // This consoles whatever current value changes that is been made on the input
+
+        // console.log(inputRef.current.focus)
+        inputRef.current.value = "";
+    }
+
+  return (
+    <div>
+        <h3>Chilaka</h3>
+        <input type="text" placeholder="Buzz..." ref={inputRef} />
+        <button onClick={handleClick}>Change Name</button>
+    </div>
+  )
+}
+
+export default RefTut
